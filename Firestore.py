6f91f86e3005eaabba6c3b357056e168de907ef7
Daniel Ekeroth <danielekeroth123321@gmail.com>
2021-04-29 09:16:33 +0200

    Changed collection name and added testFunction.

--- Firestore.py
+++ Firestore.py
@@ -17,34 +17,32 @@ class Firestore:
     def uploadPositionData(self,x,y):
         jsonData = {"x": x, "y": y}
         if self.ongoingId:
-                self.db.collection(u"mowerPositionData").document(u"{}".format(self.ongoingId)).collection(u"path").document().set(jsonData)
+                self.db.collection(u"mowerData").document(u"{}".format(self.ongoingId)).collection(u"path").document().set(jsonData)
         else:
-            self.db.collection(u"mowerPositionData").document(u"{}".format(self.time)).collection(u"path").document().set(jsonData)
+            self.db.collection(u"mowerData").document(u"{}".format(self.time)).collection(u"path").document().set(jsonData)
             self.ongoingId = self.time
 
     def uploadUltrasonicData(self,data):
         jsonData = {"value": data}
         if self.ongoingId:
-                self.db.collection(u"mowerPositionData").document(u"{}".format(self.ongoingId)).collection(u"ultraSonicData").document().set(jsonData)
+                self.db.collection(u"mowerData").document(u"{}".format(self.ongoingId)).collection(u"ultraSonicData").document().set(jsonData)
         else:
-            self.db.collection(u"mowerPositionData").document(u"{}".format(self.time)).collection(u"ultraSonicData").document().set(jsonData)
+            self.db.collection(u"mowerData").document(u"{}".format(self.time)).collection(u"ultraSonicData").document().set(jsonData)
             self.ongoingId = self.time
 
     def uploadLineFollowerData(self,data):
         jsonData = {"value": data}
         if self.ongoingId:
-                self.db.collection(u"mowerPositionData").document(u"{}".format(self.ongoingId)).collection(u"lineFollwingData").document().set(jsonData)
+                self.db.collection(u"mowerData").document(u"{}".format(self.ongoingId)).collection(u"lineFollwingData").document().set(jsonData)
         else:
-            self.db.collection(u"mowerPositionData").document(u"{}".format(self.time)).collection(u"lineFollowingData").document().set(jsonData)
+            self.db.collection(u"mowerData").document(u"{}".format(self.time)).collection(u"lineFollowingData").document().set(jsonData)
             self.ongoingId = self.time
 
     def startNewSession(self):
-        self.time = None
+        self.ongoingId = None
         
-
-if __name__ == "__main__":
-    _firestore = Firestore()
-    _firestore.uploadPositionData(3,3)
-    _firestore.uploadPositionData(4,4)
-    _firestore.uploadLineFollowerData(3)
-    _firestore.uploadUltrasonicData(5)
+    def testFIrestore(self):
+        self.uploadPositionData(3,3)
+        self.uploadPositionData(4,4)
+        self.uploadLineFollowerData(3)
+        self.uploadUltrasonicData(5)
