d41a49f7f121f4bb03e08a8a23fe153e720d809a
Daniel Ekeroth <danielekeroth123321@gmail.com>
2021-05-07 10:05:37 +0200

    Fixed message types and added websocket as class.

--- wifiClient.py
+++ wifiClient.py
@@ -6,19 +6,19 @@ except ImportError:
     import _thread as thread
 import time
 
-
 def on_message(ws, message):
-    try:
-        messageLoad = json.loads(message)
-        if (messageLoad['type'] == 'ping'):
-            data = {"type": "ping"}
-            ws.send(json.dumps(data))
-        elif (messageLoad["type"] == "disconnected"):
-            print("disconnected")
-    except:
-        print("Message doesnt have a type.")
-    finally:
-        print("Message: {}".format(message))
+        try:
+            if message:
+                if (message == 'ping'):
+                    ws.send("ping")
+                elif (message == "disconnected"):
+                    #Send data to arduino indicating that we are now
+                    #disconnected from the App meaning we go AutoDrive
+                    pass
+        except:
+            print("Message is empty")
+        finally:
+            print("Message: {}".format(message))
 
 
 def on_error(ws, error):
@@ -34,11 +34,23 @@ def on_open(ws):
         ws.send("Raspberry")
     thread.start_new_thread(run, ())
 
-
-if __name__ == "__main__":
-    ws = websocket.WebSocketApp("ws://212.25.137.196:1337",
+class WebSocket:
+    def __init__(self):
+        self.HOST = "192.168.0.93" #change this to the right HOST adress
+        self.PORT = "1337"
+        self.ws = websocket.WebSocketApp("ws://{}:{}".format(self.HOST,self.PORT),
                                 on_open=on_open,
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
-    ws.run_forever()
+
+    
+    def startSocket(self):
+        self.ws.run_forever()
+
+
+#this is used to test the class
+#remove if this class is to be used in a seperate file
+if __name__ == "__main__":
+    _websocket = WebSocket()
+    _websocket.startSocket()
